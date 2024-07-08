8ae3de39360d94fb0b0b4eb50c231a793f774f41
Dmitrii Barilkin <dimabaril@gmail.com>
2024-07-08 14:05:01 +0200

    feat: loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Component } from "react";
 import "./App.css";
 import Content from "./components/content/Content";
 import ErrorButton from "./components/errorButton/ErrorButton";
+import Loader from "./components/loader/Loader";
 import Search from "./components/search/Search";
 import { Character } from "./interfaces/interfaces";
 import { fetchSWPeople } from "./services/api";
 
 interface AppState {
   results: Character[];
+  isLoading: boolean;
 }
 
 export default class App extends Component<Record<string, never>, AppState> {
@@ -17,24 +19,29 @@ export default class App extends Component<Record<string, never>, AppState> {
 
     this.state = {
       results: [],
+      isLoading: false,
     };
   }
 
   handleSearch = async (term: string) => {
+    this.setState({ isLoading: true });
     try {
       const response = await fetchSWPeople(term);
       this.setState({ results: response });
     } catch (error) {
       console.error("Error fetching search results:", error);
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
   render = () => {
+    const { isLoading, results } = this.state;
     return (
       <>
         <Search className="search" onSearch={this.handleSearch} />
         <ErrorButton />
-        <Content results={this.state.results} />
+        {isLoading ? <Loader /> : <Content results={results} />}
       </>
     );
   };
---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+import "./Loader.scss";
+
+export default class Loader extends Component {
+  render() {
+    return <div className="loader">Loading...</div>;
+  }
+}
