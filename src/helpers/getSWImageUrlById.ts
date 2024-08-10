b0160291098a8488a81dd688593a16155243dcce
Dmitrii Barilkin <dimabaril@gmail.com>
2024-08-10 17:07:38 +0200

    chore: eslint-config-next

--- src/helpers/getSWImageUrlById.ts
+++ src/helpers/getSWImageUrlById.ts
@@ -1,11 +1,11 @@
-enum URL {
-  BASE_URL = "https://starwars-visualguide.com/assets/img/",
-  CHARACTERS = "characters/",
-  FILMS = "films/",
-  SPECIES = "species/",
-  STARSHIPS = "starships/",
-  VEHICLES = "vehicles/",
-  PLANETS = "planets/",
+export enum URL {
+  BASE_URL = "https://starwars-visualguide.com/",
+  CHARACTERS = "assets/img/characters/",
+  FILMS = "assets/img/films/",
+  SPECIES = "assets/img/species/",
+  STARSHIPS = "assets/img/starships/",
+  VEHICLES = "assets/img/vehicles/",
+  PLANETS = "assets/img/planets/",
 }
 
 export const getSWCharacterImageUrlById = (id: number | string) =>
